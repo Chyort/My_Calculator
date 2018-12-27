47eb2649bcb5fa05fd47c62eafbe41e38c318c7f
Lou Pruitt <Lpruitt2004@gmail.com>
2018-12-27 11:49:35 -0800

    click handler displays first number to the DOM

--- main.js
+++ main.js
@@ -1,61 +1,38 @@
-$(document).ready(initializeApp);
-let inputArray = [];
-function initializeApp(){
-    attachClickHandlers();
-}
+$(document).ready(init);
 
-function attachClickHandlers(){
-    $('.number').on('click', doMath);
-    $('.operator').on('click', doMath);
-    $('.equalSign').on('click', doMath);
-    $('.clear').on('click', resetApp);
-}
-
-function resetApp(){
-    inputArray = [];
-    $('.calculatorScreen').val(0);
-    console.log('inputArray: ', inputArray);
-}
-
-function doMath (num1, num2, operator){
-    
-    let input = $(this).text();
-    if(inputArray.length === 0){
-        num1 = input;
-        inputArray.push(num1);
-        $('.calculatorScreen').val(num1);
-        console.log("num1: ", num1);
-    } else if(inputArray.length > 0 && inputArray.length <= 2){
-        operator = input;
-        inputArray.push(operator);
-        $('.calculatorScreen').val(operator);
-        console.log("operator: ", operator);
-    } else if(inputArray.length === 2){
-        num2 = input;
-        inputArray.push(num2);
-        $('.calculatorScreen').val(num2);
+    function init(){
+        attachEventHandlers();
     }
-    else if(inputArray.length === 3){
-        let num1 = parseInt(inputArray[0]);
-        let num2 = parseInt(inputArray[2]);
-        let operator = inputArray[1];
-        switch(operator){
-            case '+':
-                result = num1 + num2;
-                $('.calculatorScreen').val(result);
-                break;
-            case '-':
-                result = num1 - num2;
-                $('.calculatorScreen').val(result);
-                break;
-            case '*':
-                result = num1 * num2;
-                $('.calculatorScreen').val(result);
-                break;
-            case '/':
-                result = num1 / num2;
-                $('.calculatorScreen').val(result);
+
+    function attachEventHandlers(){
+        $('.number').click(calculator.doMath);
+        $('.operator').click(calculator.doMath);
+    }
+
+    function CalculatorApp(value, inputArray){
+        this.value = value;
+        this.inputArray = inputArray;
+
+        value = null;
+        inputArray = [];
+
+        console.log('inputArray: ', inputArray, 'value: ', value);
+
+        this.doMath = function(num1, num2, operator){
+            value = $(this).text();
+            this.num1 = num1;
+            this.num2 = num2;
+            this.operator = operator;
+
+            if(inputArray.length === 0){
+                num1 = value;
+                inputArray.push(value);
+                console.log('inputArray: ', inputArray);
+                $('.calculatorScreen').val(num1);
+                console.log('num1: ', num1);
+            }
         }
-        console.log(result);
     }
-}
+
+const calculator = new CalculatorApp();
+console.log('calculator: ', calculator);
