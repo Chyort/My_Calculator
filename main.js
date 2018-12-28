8afe2b470d8def3ca589b8b25181ed5fd94db3ff
Lou Pruitt <Lpruitt2004@gmail.com>
2018-12-28 09:54:30 -0800

    added clear button functionality

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(document).ready(init);
     }
 
     function attachEventHandlers(){
+        $('.clear').click(calculator.clearArray);
         $('.number').click(calculator.doMath);
         $('.operator').click(calculator.doMath);
     }
@@ -38,6 +39,13 @@ $(document).ready(init);
 
         console.log('inputArray: ', inputArray, 'value: ', value);
 
+        this.clearArray = function(){
+            value = null;
+            inputArray = [];
+            $('.calculatorScreen').val(0);
+            console.log('inputArray: ', inputArray, 'value: ', value);
+        }
+
         this.doMath = function(num1, num2, operator){ //assign value to appropriate parameter
             value = $(this).text();
             this.num1 = num1;
@@ -45,33 +53,39 @@ $(document).ready(init);
             this.operator = operator;
 
             if(value in CalculatorApp.storedNumbers){ //check for value in storedNumbers
-                if(isNaN(inputArray[inputArray.length - 1]) && inputArray.length !== 0){//if last item if inputArray is not a number
+                if(isNaN(inputArray[inputArray.length - 1]) && inputArray.length !== 0 || isNaN(inputArray[1]) && inputArray.length > 1){//if last item if inputArray is not a number OR second item in array is NaN -----NUM2
                     num2 = value;
                     inputArray.push(num2);
                     $('.calculatorScreen').val(num2);
                     console.log('num2: ', num2);
                 }
-                if(inputArray.length === 0 || !isNaN(inputArray[inputArray.length - 1]) && num2 === undefined){ //if inputArray is a number
+                if(inputArray.length === 0 || !isNaN(inputArray[inputArray.length - 1]) && num2 === undefined){ //if inputArray is a number -----NUM1
                     num1 = value;
                     inputArray.push(num1);
+                    console.log('num1: ', num1);
 
                     if(inputArray.length >= 1){
-                        let newNum = inputArray.join("");
-                        num1 = parseFloat(newNum);
+                        let newValue = inputArray.join("");
+                        num1 = parseFloat(newValue);
                     }
                 }
-                
+
                 let displayArray = inputArray.join("");
                 $('.calculatorScreen').val(displayArray);
-                console.log('inputArray: ', inputArray, "num1: ", num1);
+                console.log('inputArray: ', inputArray);
 
-            } else if (value in CalculatorApp.storedOperators){//check for value in storedOperators
+            } else if (value in CalculatorApp.storedOperators){//check for value in storedOperators ------OPERATOR
+                let newValue = inputArray.join("");
+                num1 = parseFloat(newValue);
                 operator = value;
                 inputArray.push(operator);
 
                 if(isNaN(inputArray[inputArray.length - 1])){//check if last item of inputArray is an operator
-                    $('.calculatorScreen').val(operator);
-                    console.log('inputArray:', inputArray);
+                    
+                    inputArray.splice(0, inputArray.length - 1, num1);
+                    let newArray = inputArray.join("");
+                    $('.calculatorScreen').val(newArray);
+                    console.log('newArray:', newArray);
                 }
             }
         }
