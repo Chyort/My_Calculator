acc5c96894d06ab73cd57e97c35334899dbf7ff3
Lou Pruitt <Lpruitt2004@gmail.com>
2019-01-03 02:19:46 -0800

    removed console.logs

--- main.js
+++ main.js
@@ -40,61 +40,64 @@ $(document).ready(init);
             '0':0
         }
 
-        console.log('inputArray: ', inputArray, 'value: ', value);
-
         this.clearArray = function(){
             value = null;
             inputArray = [];
             $('.calculatorScreen').val(0);
-            console.log('inputArray: ', inputArray, 'value: ', value);
         }
 
         this.clearEntry = function(){
+
             if(inputArray.length > 1 && typeof inputArray[inputArray.length - 1] === "string"){
                 inputArray.pop();
                 $('.calculatorScreen').val(inputArray.join(""));
+
             } else if (inputArray.length >= 2 && inputArray[inputArray.length - 1] !== "" || inputArray.length === 1 && typeof inputArray[inputArray.length - 1] === "number" && isNaN(inputArray[0])){
                 inputArray[inputArray.length - 1] = "" + inputArray[inputArray.length - 1];
                 let stringNum = inputArray[inputArray.length - 1];
                 stringNum = stringNum.slice(0, -1);
                 inputArray[inputArray.length - 1] = stringNum;
                 $('.calculatorScreen').val(inputArray.join(""));
+
             } else if (inputArray.length > 0){
+
                 if(typeof inputArray[inputArray.length -1] === "number"){
                     let stringNum = "" + inputArray[inputArray.length - 1];
+
                     if (stringNum.length > 1){
                         stringNum = stringNum.slice(0, -1);
                         inputArray[inputArray.length - 1] = stringNum;
                         $('.calculatorScreen').val(inputArray.join(""));
+
                     } else {
                         inputArray = [];
                         $('.calculatorScreen').val(0);
                     }
+
                 } else if (typeof inputArray[inputArray.length -1] === "string"){
                     let stringNum = inputArray[inputArray.length - 1];
+
                     if(stringNum.length > 1){
                         stringNum = stringNum.slice(0, -1);
                         inputArray[inputArray.length - 1] = stringNum;
                         $('.calculatorScreen').val(inputArray.join(""));
+
                     } else {
                         inputArray = [];
                         $('.calculatorScreen').val(0);
                     }
+
                 } else {
                     inputArray = [];
                     $('.calculatorScreen').val(0);
                 }
             }
-            console.log("inputArray: ", inputArray);
         }
 
         this.doMath = function(num1, num2, operator){ //assign value to appropriate parameter
             value = $(this).text();
             let inputArrayString = "" + inputArray;
             let opsRegex = inputArrayString.match(/[\+\-\*\/]/g);
-          
-          
-          
             this.num1 = num1;
             this.num2 = num2;
             this.operator = operator;
@@ -105,55 +108,51 @@ $(document).ready(init);
                     num2 = value;
                     inputArray.push(num2);
                     $('.calculatorScreen').val(num2);
-                    console.log('num2: ', num2);
 
                 } else if (inputArray.length === 0 && value !== "." || !isNaN(inputArray[inputArray.length - 1]) && num2 === undefined && value !== "." || num2 === undefined && inputArray.indexOf(".") >= 1 && value !== "."){ //if last item of inputArray is a number -----NUM1
 
                     if(inputArray[0] === 0){
                         inputArray = [];
                     }
+
                     num1 = value;
                     inputArray.push(num1);
-                    console.log('num1: ', num1);
 
                     if(inputArray.length >= 1){
                         let newValue = inputArray.join("");
                         num1 = parseFloat(newValue);
                     }
                   
-                } else if (num1 && inputArray.indexOf(".") > -1) { //check for decimal in inputArray
-                    console.log(value, " is in the array!");
-
-                } else {
+                } else { //check for decimal in inputArray
                     let decimal = value;
                     inputArray.push(decimal);
-                    console.log("inputArray: ", inputArray);
                 }
 
                 let displayArray = inputArray.join("");
                 $('.calculatorScreen').val(displayArray);
-                console.log('inputArray: ', inputArray);
 
             } else if (value in calculator.storedOperators){//check for value in storedOperators ------OPERATOR
+
                 if(inputArray.length === 0){
                     inputArray[0] = 0;
+
                 } else if (inputArrayString.includes(opsRegex)){
                     calculator.evaluate();
                 }
+
                 let newValue = inputArray.join("");
                 num1 = parseFloat(newValue);
                 operator = value;
                 inputArray.push(operator);
 
                 if(isNaN(inputArray[inputArray.length - 1])){//check if last item of inputArray is an operator
-                    
                     inputArray.splice(0, inputArray.length - 1, num1);
                     let newArray = inputArray.join("");
                     $('.calculatorScreen').val(newArray);
-                    console.log('newArray:', newArray);
                 }
             }
         }
+
         this.evaluate = function(){
             let newValue = [];
             let value = null;
@@ -175,7 +174,6 @@ $(document).ready(init);
                         newValue = newValue.join("");
                         num2 = parseFloat(newValue);
                         inputArray.splice(2, itemsToRemove, num2);
-                        console.log('inputArray: ', inputArray);
                     }
                 }
             }
@@ -184,10 +182,12 @@ $(document).ready(init);
                 num1 = inputArray[0];
                 num2 = CalculatorApp.prototype.num2;
                 operator= CalculatorApp.prototype.operator;
+
             } else if(inputArray.length === 1 && CalculatorApp.prototype.operator === undefined){
                 num1 = parseFloat(num1);
                 num2 = 0;
                 operator = "+";
+
             } else if (inputArray.length === 2){
                 num2 = inputArray[0];
             }
@@ -215,12 +215,10 @@ $(document).ready(init);
             CalculatorApp.prototype.num1 = num1;
             CalculatorApp.prototype.num2 = num2;
             CalculatorApp.prototype.operator = operator;
-            console.log("calculator prototype: ", CalculatorApp.prototype);
 
             newNum1 = (num1 + '').replace('.', '').length;
             newNum2 = (num2 + '').replace('.', '').length;
             newValueDigits = (value + '').replace('.', '').length;
-
 
             if(newNum1 + newNum2 < newValueDigits){
                 value = parseFloat(value.toFixed(newNum1 + newNum2 - 1));
@@ -229,10 +227,7 @@ $(document).ready(init);
             num1 = value;
             inputArray = [value];
             $('.calculatorScreen').val(value);
-            console.log('typeof inputArray: ', typeof inputArray[2]);
-            console.log('= : clicked', 'inputArray: ', inputArray);
         }
     }
 
 const calculator = new CalculatorApp();
-console.log('calculator: ', calculator);
